65efecb30401687136835e320b2fde7ad510aa72
Ethan Rutt <ethan.rutt2032@gmail.com>
2023-12-01 11:04:39 -0600

    removed unnecessary files

--- day1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ethanrutt/personalprojects/ethanrutt_adventofcode_2023/day1
-# Build directory: /home/ethanrutt/personalprojects/ethanrutt_adventofcode_2023/day1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
