c623f04ee06ead2ee0533076c27e60da5e70336f
Ethan Rutt <ethan.rutt2032@gmail.com>
2023-12-10 15:50:29 -0600

    day3 part 1 solved

--- day3/solve1.cpp
+++ day3/solve1.cpp
@@ -1,31 +1,97 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
+#include <string>
+#include <set>
 
 using namespace std;
 
+const string NUMS = "0123456789";
+const string SYMBOLS = "#$%&*+-/=@";
+const set<char> SYMBOLS_SET = {'#', '$', '%', '&', '*', '+', '-', '/', '=', '@'};
+
+pair<int, int> find_num(int start_pos, string s) {
+    int start;
+    int end;
+    try {
+        start = s.find_first_of(NUMS, start_pos);
+        end = s.find_first_not_of(NUMS, start);
+    } catch (const out_of_range& e) {
+        return {-1, -1};
+    }
+
+    if (end == -1) {
+        end = s.size();
+    }
+
+    return {start, end};
+}
+
+bool check_num(string above, string curr, string below, pair<int, int> range) {
+    const int start_idx = range.first <= 0 ? range.first : range.first - 1;
+    const int end_idx = range.second >= curr.size() ? range.second - 1 : range.second;
+    if (!above.empty()) {
+        for (int i = start_idx; i <= end_idx; i++) {
+            if (SYMBOLS_SET.find(above[i]) != SYMBOLS_SET.end()) {
+                return true;
+            }
+        }
+    }
+
+    if (!below.empty()) {
+        for (int i = start_idx; i <= end_idx; i++) {
+            if (SYMBOLS_SET.find(below[i]) != SYMBOLS_SET.end()) {
+                return true;
+            }
+        }
+    }
+
+    if (
+        SYMBOLS_SET.find(curr[start_idx]) != SYMBOLS_SET.end() ||
+        SYMBOLS_SET.find(curr[end_idx]) != SYMBOLS_SET.end()
+    ) {
+        return true;
+    }
+
+    return false;
+}
+
+void check_line(string above, string curr, string below, int& sum) {
+    int idx = 0;
+    pair<int, int> range = find_num(idx, curr);
+
+    while (range.first != -1) {
+        idx = range.second;
+        string num = curr.substr(range.first, range.second - range.first);
+        if (check_num(above, curr, below, range)) {
+            sum += atoi(num.c_str());
+        }
+        range = find_num(idx, curr);
+    }
+
+}
 
 int main() {
-    vector<string> input(1001);
+    vector<string> input(141);
     unsigned int i = 0;
+    int sum = 0;
 
     while (getline(cin, input[i++]));
 
-    /**
-     *
-     * find number with find_first_of("0123456789")
-     * do find_first_not_of("0123456789") so that I can isolate the number with start and end indexes
-     * get first, last index of number, as well as line number
-     *
-     * check all elements around the number to determine if it's part of schematic
-     * need to handle corner checks
-     * if part of schematic -> add to sum
-     *
-     */
+    for (i = 0; i < input.size(); i++) {
+        if (i == 0) {
+            check_line(string(), input[i], input[i+1], sum);
+        }
+        else if (i == input.size() - 1) {
+            check_line(input[i-1], input[i], string(), sum);
+        }
+        else {
+            check_line(input[i-1], input[i], input[i+1], sum);
+        }
+    }
 
-
-
-
+    cout << "sum: " << sum << endl;
 
     return 0;
-
 }
+
